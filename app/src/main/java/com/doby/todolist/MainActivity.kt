641a7ed7125846ccef9cddd222b58fc86cc318b5
mszab0310 <mszabi0310@gmail.com>
2021-08-10 12:38:17 +0300

    Add button listeners

--- app/src/main/java/com/doby/todolist/MainActivity.kt
+++ app/src/main/java/com/doby/todolist/MainActivity.kt
@@ -2,10 +2,30 @@ package com.doby.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var todoAdapter : TodoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        rvToDoItems.adapter = todoAdapter
+        rvToDoItems.layoutManager = LinearLayoutManager(this)
+
+        btnAddToDo.setOnClickListener {
+            val todoTitle = etToDoTitle.text.toString()
+            if(todoTitle.isNotEmpty()){
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etToDoTitle.text.clear()
+            }
+        }
+
+        btnDelete.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
     }
 }
